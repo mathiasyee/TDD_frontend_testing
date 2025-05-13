e354b8e9e95ad53d59afcccd898e700329e79e95
mathiasyee <mathias.yeemayor@hotmail.com>
2025-05-13 23:48:18 +0200

    initial commit

---
+++ src/components/CustomButton.test.tsx
@@ -0,0 +1,44 @@
+
+import { render, fireEvent } from "@testing-library/react";
+import CustomButton, { ButtonState } from "./CustomButton";
+import "@testing-library/jest-dom";
+
+describe("CustomButton", () => {
+ test("renders with correct text and idle class", () => {
+  const buttonText = "signup";
+  const { getByRole } = render(<CustomButton buttonText={buttonText} />);
+  const button = getByRole("button", { name: buttonText });  // Den här raden ger ett problem
+
+  expect(button).toHaveValue(buttonText);  // Korrigerad förväntan
+  expect(button).toHaveClass(ButtonState.idle);
+});
+
+  test("has idle class by default", () => {
+    const { getByRole } = render(<CustomButton buttonText="Test" />);
+    expect(getByRole("button")).toHaveClass(ButtonState.idle);
+  });
+
+  test("changes to hover class on mouse enter", () => {
+    const { getByRole } = render(<CustomButton buttonText="Test" />);
+    const button = getByRole("button");
+
+    fireEvent.mouseEnter(button);
+    expect(button).toHaveClass(ButtonState.hover);
+  });
+
+  test("changes to clicked class on click", () => {
+    const { getByRole } = render(<CustomButton buttonText="Test" />);
+    const button = getByRole("button");
+
+    fireEvent.click(button);
+    expect(button).toHaveClass(ButtonState.clicked);
+  });
+
+  test("has the correct class name on click", () => {
+    const { getByRole } = render(<CustomButton buttonText="Test" />);
+    const button = getByRole("button");
+
+    fireEvent.click(button);
+    expect(button).toHaveClass(ButtonState.clicked);
+  });
+});
---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,33 @@
+/* TODO - Have we interacted with the button?
+*  Check for states, for hover, clicked and idle
+* Classname TEST
+*/
+
+import { useState } from "react"
+
+//ENUM -3 different states for button
+// IDLE - HOVER - CLICKED
+export enum ButtonState {
+  idle = "idle",
+  hover = "hover",
+  clicked = "clicked"
+}
+
+export interface CustomButtonProps {
+  buttonText: string;
+}
+
+export default function CustomButton(props: CustomButtonProps) {
+  const [buttonState, setButtonState] = useState<ButtonState>(ButtonState.idle);
+
+  return (
+    <input
+      type="button"
+      value={props.buttonText}
+      className={buttonState}
+      onMouseLeave={() => setButtonState(ButtonState.idle)}
+      onMouseEnter={() => setButtonState(ButtonState.hover)}
+      onClick={() => setButtonState(ButtonState.clicked)}
+    />
+  );
+}
